eceeacb72a08c96b397c5faae160aba65276c471
Myngatte <zhouming2004@gmail.com>
2025-04-15 17:56:17 +0200

    layout completov1

--- Chazam/app/Providers/AppServiceProvider.php
+++ Chazam/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Compartir variables de usuario con todas las vistas
+        View::composer('*', function ($view) {
+            if (Auth::check()) {
+                $user = Auth::user();
+                $view->with('racha', $user->racha);
+                $view->with('puntos', $user->puntos);
+            } else {
+                $view->with('racha', 0);
+                $view->with('puntos', 0);
+            }
+        });
     }
 }
