ed0902843d0976d7029bbb000aab79ba2654987b
Marc Martinez <mxrcxs.mxrtxnez@gmail.com>
2025-04-21 20:00:56 +0200

    Arreglo modal.js

--- Chazam/public/js/modals.js
+++ Chazam/public/js/modals.js
@@ -10,20 +10,20 @@ function closeModal(modalId) {
 
 // Función para abrir el modal de editar con datos prellenados
 function openEditModal(user) {
+    // Cargar los datos del usuario en el modal
     document.getElementById('edit_username').value = user.username;
     document.getElementById('edit_nombre').value = user.nombre;
     document.getElementById('edit_apellido').value = user.apellido;
     document.getElementById('edit_fecha_nacimiento').value = user.fecha_nacimiento;
     document.getElementById('edit_email').value = user.email;
     document.getElementById('edit_descripcion').value = user.descripcion;
+    document.getElementById('edit_id_nacionalidad').value = user.id_nacionalidad;
 
-    // Preseleccionar la nacionalidad
-    const nacionalidadSelect = document.getElementById('edit_id_nacionalidad');
-    nacionalidadSelect.value = user.id_nacionalidad;
-
-    // Actualizar la acción del formulario
+    // Actualizar la acción del formulario para enviar la solicitud al endpoint correcto
     const editForm = document.getElementById('editForm');
     editForm.action = `/admin/${user.id_usuario}`;
 
-    openModal('editModal');
+    // Abrir el modal
+    const editModal = new bootstrap.Modal(document.getElementById('editModal'));
+    editModal.show();
 }
