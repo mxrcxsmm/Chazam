277b913af08caf13a6d92e5447b4dccc758455c6
Dmasc67 <sergi.masip2004@gmail.com>
2025-05-15 16:20:35 +0200

    alo

--- Chazam/SQL/bddd.sql
+++ Chazam/SQL/bddd.sql
@@ -17,7 +17,6 @@
 --
 -- Table structure for table `cache`
 --
-
 DROP TABLE IF EXISTS `cache`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
